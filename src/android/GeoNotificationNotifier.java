c4c1873aa61587df7b826c4d5a572fe043c304fa
Richard Lewin <richardlewin@Richards-MacBook-Pro.local>
2017-11-30 14:20:51 +0000

    - Updated code to handle new Android permission system - Code cleanup - Bumped version number

--- src/android/GeoNotificationNotifier.java
+++ src/android/GeoNotificationNotifier.java
@@ -8,7 +8,6 @@ import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 
 public class GeoNotificationNotifier {
--- src/android/GeofencePlugin.java
+++ src/android/GeofencePlugin.java
@@ -1,13 +1,17 @@
 package com.cowbell.cordova.geofence;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.util.Log;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PermissionHelper;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,6 +25,20 @@ public class GeofencePlugin extends CordovaPlugin {
     private Context context;
     protected static Boolean isInBackground = true;
     public static CordovaWebView webView = null;
+
+    private class Action {
+        public String action;
+        public JSONArray args;
+        public CallbackContext callbackContext;
+
+        public Action(String action, JSONArray args, CallbackContext callbackContext) {
+            this.action = action;
+            this.args = args;
+            this.callbackContext = callbackContext;
+        }
+    }
+
+    private Action executedAction;
 
     /**
      * @param cordova
@@ -42,6 +60,8 @@ public class GeofencePlugin extends CordovaPlugin {
             CallbackContext callbackContext) throws JSONException {
         Log.d(TAG, "GeofencePlugin execute action: " + action + " args: "
                 + args.toString());
+
+        executedAction = new Action(action, args, callbackContext);
 
         if (action.equals("addOrUpdate")) {
             List<GeoNotification> geoNotifications = new ArrayList<GeoNotification>();
@@ -66,7 +86,7 @@ public class GeofencePlugin extends CordovaPlugin {
                     .getWatched();
             callbackContext.success(Gson.get().toJson(geoNotifications));
         } else if (action.equals("initialize")) {
-            callbackContext.success();
+            initialize(callbackContext);
         } else if (action.equals("deviceReady")) {
             deviceReady();
             callbackContext.success();
@@ -74,6 +94,10 @@ public class GeofencePlugin extends CordovaPlugin {
             return false;
         }
         return true;
+    }
+
+    public boolean execute(Action action) throws JSONException {
+        return execute(action.action, action.args, action.callbackContext);
     }
 
     private GeoNotification parseFromJSONObject(JSONObject object) {
@@ -105,7 +129,7 @@ public class GeofencePlugin extends CordovaPlugin {
             webView.sendJavascript(js);
         }
     }
-    
+
     @Override
     public void onNewIntent(Intent intent) {
         String data = intent.getStringExtra("geofence.notification.data");
@@ -118,5 +142,46 @@ public class GeofencePlugin extends CordovaPlugin {
             webView.sendJavascript(js);
         }
         super.onNewIntent(intent);
+    }
+
+    private void initialize(CallbackContext callbackContext) {
+        String[] permissions = {
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        };
+
+        if (!hasPermissions(permissions)) {
+            PermissionHelper.requestPermissions(this, 0, permissions);
+        } else {
+            callbackContext.success();
+        }
+    }
+
+    private boolean hasPermissions(String[] permissions) {
+        for (String permission : permissions) {
+            if (!PermissionHelper.hasPermission(this, permission)) return false;
+        }
+
+        return true;
+    }
+
+    public void onRequestPermissionResult(int requestCode, String[] permissions,
+                                          int[] grantResults) throws JSONException {
+        PluginResult result;
+
+        if (executedAction != null) {
+            for (int r:grantResults) {
+                if (r == PackageManager.PERMISSION_DENIED) {
+                    Log.d(TAG, "Permission Denied!");
+                    result = new PluginResult(PluginResult.Status.ILLEGAL_ACCESS_EXCEPTION);
+                    executedAction.callbackContext.sendPluginResult(result);
+                    executedAction = null;
+                    return;
+                }
+            }
+            Log.d(TAG, "Permission Granted!");
+            execute(executedAction);
+            executedAction = null;
+        }
     }
 }
